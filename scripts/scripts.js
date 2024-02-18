fbd963f2ec70fcf7988820f24430b5658cdb92a3
arifulislamk <ariful18374@gmail.com>
2024-02-18 23:38:45 +0600

    select only 4 seat and more

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,53 +1,59 @@
-
-let count = 0 ;
+let count = 0;
+let selected = 0;
 const alltickets = document.getElementsByClassName('seats');
 for (const seat of alltickets) {
     seat.addEventListener('click', function (e) {
-        console.log(e.target.innerText)
-        const clickedSeat = e.target.innerText;
-        console.log(clickedSeat, 'clieked')
-        setBackgroundColorById(clickedSeat);
-
-        const seatValue = getValueById('seat-left');
-        const avaleavail = seatValue - 1;
-        setTextById('seat-left', avaleavail);
-
-        makeTable(clickedSeat);
-
-        const total = getValueById('total-price')
-        const newtotal = total + 550;
-        setTextById('total-price', newtotal)
-
-        const grandtotal = getValueById('grand-total');
-        let newGarndtotal = grandtotal + 550;
-        setTextById('grand-total', newGarndtotal
-        )   
-        
-        count = count + 1 ;
-        if(count === 4){
+        count = count + 1;
+        if (count === 2) {
             const applybtn = document.getElementById('apply-btn');
             applybtn.removeAttribute('disabled');
             applybtn.classList.add('bg-bg-green')
         }
+        if (count < 5) {
+            console.log(e.target.innerText)
+            const clickedSeat = e.target.innerText;
+            console.log(clickedSeat, 'clieked')
+            setBackgroundColorById(clickedSeat);
+
+            const seatValue = getValueById('seat-left');
+            const avaleavail = seatValue - 1;
+            setTextById('seat-left', avaleavail);
+
+            const selectedSeat = getValueById('selected-seat');
+            selected = selected + 1;
+            setTextById('selected-seat', selected);
+            setBackgroundColorById('selected-seat')
+
+            makeTable(clickedSeat);
+
+            const total = getValueById('total-price')
+            const newtotal = total + 550;
+            setTextById('total-price', newtotal)
+
+            const grandtotal = getValueById('grand-total');
+            let newGarndtotal = grandtotal + 550;
+            setTextById('grand-total', newGarndtotal
+            )
+        }
     })
 }
 
-function apply(){
-    const inputText = document.getElementById('cupon-input').value ;
-    if(inputText === 'NEW15'){
-        const grand = document.getElementById('grand-total').innerText ;
-        const discount = grand/100 * 15 ;
-        const updateGrand = grand - discount ;
+function apply() {
+    const inputText = document.getElementById('cupon-input').value;
+    if (inputText === 'NEW15') {
+        const grand = document.getElementById('grand-total').innerText;
+        const discount = grand / 100 * 15;
+        const updateGrand = grand - discount;
         setTextById('grand-total', updateGrand);
         const btn = document.getElementById('apply-btn');
         btn.classList.add('hidden');
         const input = document.getElementById('cupon-input');
         input.classList.add('hidden');
     }
-    else if(inputText === 'Couple 20'){
-        const grand = document.getElementById('grand-total').innerText ;
-        const discount = grand/100 * 20 ;
-        const updateGrand = grand - discount ;
+    else if (inputText === 'Couple 20') {
+        const grand = document.getElementById('grand-total').innerText;
+        const discount = grand / 100 * 20;
+        const updateGrand = grand - discount;
         setTextById('grand-total', updateGrand);
         const btn = document.getElementById('apply-btn');
         btn.classList.add('hidden');
@@ -55,20 +61,30 @@ function apply(){
         input.classList.add('hidden');
     }
 }
-// const inputText = document.getElementById('cupon-input') ;
-// const text = document.getElementById('text') ;
-// console.log(text.innerText) ;
-// inputText.addEventListener('keyup' , function inputText (event){
-//     // console.log(inputText.value)
-//     const text =  document.getElementById('text');
-//     text.innerText = event.target.value ;
-    
-// })
-// const applybtn = document.getElementById('apply-btn');
-// applybtn.addEventListener('click', function(){
-//     const grandtotal = document.getElementById('grand-total');
-    
-// })
+function succeesful() {
+    const numinputText = document.getElementById('numberid').value;
+    const naminputText = document.getElementById('nameid').value;
+    if (naminputText.length > 0 && numinputText.length > 0) {
+        const hiddendiv = document.getElementById('succeesful');
+        hiddendiv.classList.remove('hidden');
+        const purchase = document.getElementById('purchase');
+        purchase.classList.add('hidden');
+        const Cuppon = document.getElementById('Cuppon');
+        Cuppon.classList.add('hidden');
+        const header = document.getElementById('header');
+        header.classList.add('hidden')
+    }
+}
+function continued() {
+    const hiddendiv = document.getElementById('succeesful');
+    hiddendiv.classList.add('hidden');
+    const purchase = document.getElementById('purchase');
+    purchase.classList.remove('hidden');
+    const Cuppon = document.getElementById('Cuppon');
+    Cuppon.classList.remove('hidden');
+    const header = document.getElementById('header');
+    header.classList.remove('hidden')
+}
 
 
 
