8cfd5fedf59f298adfc0070f8754515599c077ea
arifulislamk <ariful18374@gmail.com>
2024-02-19 00:55:08 +0600

    validation part

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,10 +4,10 @@ const alltickets = document.getElementsByClassName('seats');
 for (const seat of alltickets) {
     seat.addEventListener('click', function (e) {
         count = count + 1;
-        if (count === 2) {
+        if (count === 4) {
             const applybtn = document.getElementById('apply-btn');
             applybtn.removeAttribute('disabled');
-            applybtn.classList.add('bg-bg-green')
+            applybtn.classList.add('bg-bg-green');
         }
         if (count < 5) {
             console.log(e.target.innerText)
@@ -34,6 +34,9 @@ for (const seat of alltickets) {
             let newGarndtotal = grandtotal + 550;
             setTextById('grand-total', newGarndtotal
             )
+            const nextbtn = document.getElementById('next-btn');
+            nextbtn.setAttribute("onclick", "succeesful()");
+            nextbtn.removeAttribute("disabled");
         }
     })
 }
@@ -45,6 +48,18 @@ function apply() {
         const discount = grand / 100 * 15;
         const updateGrand = grand - discount;
         setTextById('grand-total', updateGrand);
+
+        const p1 = document.createElement('p');
+        p1.innerText = "Discount total";
+        p1.classList.add('lg:px-5', 'px-3', 'py-2','text-center');
+        const p2 = document.createElement('p');
+        p2.innerText = discount ;
+        p2.classList.add('lg:px-5', 'px-3', 'py-2','text-center');
+
+        const cuppon = document.getElementById('cuppon');
+        cuppon.appendChild(p1);
+        cuppon.appendChild(p2);
+        // 
         const btn = document.getElementById('apply-btn');
         btn.classList.add('hidden');
         const input = document.getElementById('cupon-input');
@@ -55,6 +70,18 @@ function apply() {
         const discount = grand / 100 * 20;
         const updateGrand = grand - discount;
         setTextById('grand-total', updateGrand);
+
+        const p1 = document.createElement('p');
+        p1.innerText = "Discount total";
+        p1.classList.add('lg:px-5', 'px-3', 'py-2','text-center');
+        const p2 = document.createElement('p');
+        p2.innerText = discount ;
+        p2.classList.add('lg:px-5', 'px-3', 'py-2','text-center');
+
+        const cuppon = document.getElementById('cuppon');
+        cuppon.appendChild(p1);
+        cuppon.appendChild(p2);
+
         const btn = document.getElementById('apply-btn');
         btn.classList.add('hidden');
         const input = document.getElementById('cupon-input');
